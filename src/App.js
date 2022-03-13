e6e77a5448a266418aeb24bd6158dffee6efb154
AJ Kingston <akingstonlewis@gmail.com>
2022-03-13 15:44:10 -0700

    awd

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import Navbar from './components/Navbar';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './components/pages/HomePage/Home';
 import Footer from './components/pages/Footer/Footer';
 
